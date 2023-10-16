a9db991a3f10a6159b31420ee343f4c4fc343085
Docta Rhyf <drrhyf@gmail.com>
2023-10-16 13:40:19 +0200

    working on admin int

--- src/pages/PageAdmin.jsx
+++ src/pages/PageAdmin.jsx
@@ -7,6 +7,15 @@ import * as fa from "@fortawesome/free-solid-svg-icons";
 
 export default function PageAdmin({}) {
   const [admin, setadmin] = useState(false);
+  const [diapos, setdiapos] = useState([
+    ...Array(5).fill({
+      title: "Title",
+      description: "desc",
+      button: "button",
+      path: "path",
+      pic: "",
+    }),
+  ]);
 
   useState(() => {
     loadAdmin();
@@ -14,9 +23,18 @@ export default function PageAdmin({}) {
 
   async function loadAdmin() {
     let u = localStorage.getItem("fad");
-
-    console.log(u);
+    setadmin(JSON.parse(u));
   }
 
-  return <div className="text-center m-8 ">Admin</div>;
+  if (admin) {
+    return (
+      <div>
+        {Object.values(diapos).map((it, i) => (
+          <div key={i}>{JSON.stringify(it)}</div>
+        ))}
+      </div>
+    );
+  }
+
+  return <div className="text-center m-8 ">Login</div>;
 }
