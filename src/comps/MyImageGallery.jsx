82cfe691db495af9c8489a39bdcf4b5857441a39
Docta Rhyf <drrhyf@gmail.com>
2023-10-17 03:11:12 +0200

    added new image gallery

--- src/comps/MyImageGallery.jsx
+++ src/comps/MyImageGallery.jsx
@@ -1,6 +1,33 @@
 import React, { useEffect, useRef, useState } from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 import "../App.css";
+
+const imgz = [
+  {
+    original:
+      "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_large/v1/editorial/segment_review/hero_image/2023-Alfa-Romeo-Stelvio-Quadrifoglio-SUV-Blue-1001x565-%281%29.jpg",
+    thumbnail:
+      "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_large/v1/editorial/segment_review/hero_image/2023-Alfa-Romeo-Stelvio-Quadrifoglio-SUV-Blue-1001x565-%281%29.jpg",
+  },
+  {
+    original:
+      "https://1721181113.rsc.cdn77.org/data/images/full/26315/space-satellite-orbiting-the-earth-3d-rendering.jpg",
+    thumbnail:
+      "https://1721181113.rsc.cdn77.org/data/images/full/26315/space-satellite-orbiting-the-earth-3d-rendering.jpg",
+  },
+  {
+    original: "https://i.ytimg.com/vi_webp/_NX8F9FBvg0/maxresdefault.webp",
+    thumbnail: "https://i.ytimg.com/vi_webp/_NX8F9FBvg0/maxresdefault.webp",
+  },
+  {
+    original:
+      "https://morethanshipping.com/wp-content/uploads/2020/10/shipping-container-types.jpg",
+    thumbnail:
+      "https://morethanshipping.com/wp-content/uploads/2020/10/shipping-container-types.jpg",
+  },
+];
 
 export default function MyImageGallery({ images }) {
   const [content, setcontent] = useState([
@@ -42,26 +69,7 @@ export default function MyImageGallery({ images }) {
 
   return (
     <>
-      swipper
-      {/*  <Slider autoplay={2600}>
-        {content.map((item, index) => (
-          <div
-            key={index}
-            style={{
-              backgroundSize: "90%",
-              background: `url('${item.image}') no-repeat center center`,
-            }}
-          >
-            <div className=" bg-black/40 space-y-1 md:mx-auto shadow-md  w-fit mt-[120pt]  text-white p-2 md:p-4 rounded-md">
-              <h1 className="text-3xl  shadow-black text-clip">{item.title}</h1>
-              <p>{item.description}</p>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-md">
-                {item.button}
-              </button>
-            </div>
-          </div>
-        ))}
-      </Slider> */}
+      <ImageGallery items={imgz} autoPlay={true} useTranslate3D={true} />;
     </>
   );
 }
